7f60181b1b98cc75b69ba44ac221c6c0154fdb37
lilianzhu <zhujiali@qiniu.com>
2020-06-16 15:54:49 +0800

    trigger pipeline

--- cici-backend.go
+++ cici-backend.go
@@ -7,7 +7,7 @@ import (
 
 func hello(w http.ResponseWriter, req *http.Request) {
 
-	fmt.Fprintf(w, "hi, my name is cici\n")
+	fmt.Fprintf(w, "hello, my name is cici\n")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
